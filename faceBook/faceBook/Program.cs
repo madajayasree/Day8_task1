8166a3809845328e5b40e32e54ce96236c020bf3
madajayasree <93071345+madajayasree@users.noreply.github.com>
2021-11-02 22:14:14 +0530

    Add files via upload

---
+++ faceBook/faceBook/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   
+            users[] obj_users;
+            Console.WriteLine("Enter number of users");
+            int obj_count;
+            obj_count = int.Parse(Console.ReadLine());
+            obj_users = new users[obj_count];
+            for (int i = 0; i < obj_count; i++)
+            {
+                string user_Fname;
+                string user_LastName;
+                string user_email;
+                long user_mobileNo;
+                DateTime user_dob;
+                
+                Console.WriteLine("Enter Fname: ");
+                user_Fname = Console.ReadLine();
+
+                Console.WriteLine("Enter Lastname: ");
+                user_LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Email: ");
+                user_email = Console.ReadLine();
+
+                Console.WriteLine("Enter MobileNumber: ");
+                user_mobileNo = Convert.ToInt64(Console.ReadLine());
+
+                Console.WriteLine("Enter Date of Birth: ");
+                user_dob = Convert.ToDateTime(Console.ReadLine());
+
+                users obj = new users(user_Fname, user_LastName, user_email, user_mobileNo, user_dob);
+
+
+
+                if (!obj_users.Contains(obj))
+                {
+                    obj_users[i] = obj;
+                }
+                else
+                {
+                    Console.WriteLine("accouont is already exits re_enter the account details: ");
+                    i--;
+                }
+            }
+
+            foreach(var item in obj_users)
+            {
+                Console.WriteLine(item);
+            }
+             
+
+        }
+    }
+}
---
+++ faceBook/faceBook/users.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faceBook
+{
+    class users
+    {
+        //Fields
+        string Fname;
+        string LastName; 
+        string email;
+        long mobileNo;
+        DateTime dob;
+
+      
+        //Properties
+        public string Fname1 { get => Fname; set => Fname = value; }
+        public string LastName1 { get => LastName; set => LastName = value; }
+        public string Email { get => email; set => email = value; }
+        public long MobileNo { get => mobileNo; set => mobileNo = value; }
+        public DateTime Dob { get => dob; set => dob = value; }
+
+        //Constructor
+        public users()
+        {
+        }
+
+        public users(string fname, string lastName, string email, long mobileNo, DateTime dob)
+        {
+            Fname = fname;
+            LastName = lastName;
+            this.email = email;
+            this.mobileNo = mobileNo;
+            this.dob = dob;
+        }
+
+        //override String
+        public override string ToString()
+        {
+            return String.Format("Fname is : {0}   lname is : {1}   email is : {2}  MobileNumber is: {3}  dob is: {4} ", Fname1,LastName1,Email,MobileNo,Dob);
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            users u = (users)obj;
+            return this.email.Equals(u.email) && this.mobileNo.Equals(u.mobileNo);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+
+    }
+}
